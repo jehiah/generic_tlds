62b966a4ba1a92ca3657acb9fc740b89004d1f82
Jehiah Czebotar <jehiah@gmail.com>
2024-04-01 10:29:41 -0400

    sync.sh: update

--- sync.sh
+++ sync.sh
@@ -14,8 +14,8 @@ git status
 FILES_CHANGED=$(git diff --staged --name-only | wc -l)
 echo "FILES_CHANGED: ${FILES_CHANGED}"
 # if more than one changed commit it
-if [[ "${FILES_CHANGED}" -gt 1 ]]; then
-    DT=$(TZ=America/New_York date "+%Y-%m-%d %H:%M")
-    git commit -a -m "sync: ${DT}"
+if [[ "${FILES_CHANGED}" -ge 1 ]]; then
+    DT=$(TZ=Etc/UTC date "+%Y-%m-%d %H:%M")
+    git commit -a -m "sync: ${DT} UTC"
     git push origin master
 fi
